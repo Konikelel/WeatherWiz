4f8daded38943bb68697186e188fbc21d87f848f
Konikelel <wiktoralbinowski@gmail.com>
2024-07-27 11:21:02 +0200

    add all main components and style them inside app container

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./navbar/navbar.component";
+import {WeeklyForecastComponent} from "./weekly-forecast/weekly-forecast.component";
+import {HourlyForecastComponent} from "./hourly-forecast/hourly-forecast.component";
+import {WeatherDataComponent} from "./weather-data/weather-data.component";
+import {WeatherNowComponent} from "./weather-now/weather-now.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, WeeklyForecastComponent, HourlyForecastComponent, WeatherDataComponent, WeatherNowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ angular/src/app/weather-data/weather-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-data.component.html',
+  styleUrl: './weather-data.component.scss'
+})
+export class WeatherDataComponent {
+
+}
